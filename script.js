c33a7787f0fe0fcc7879aaf3d04082366e4cfa3d
Juan Pablo Torres <jptorressnl19@gmail.com>
2021-09-20 17:34:07 -0600

    Added display and clear filters functions

--- script.js
+++ script.js
@@ -1,36 +1,54 @@
 filterListings("all");
 
 function filterListings (c) {
-    var x, i;
-    x = document.getElementsByClassName("card");
+    var cards, i;
+    cards = document.getElementsByClassName("card");
     if (c == "all") c = "";
-    for (i = 0; i < x.length; i++) {
-        removeClass(x[i], "show");
-        if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+    for (i = 0; i < cards.length; i++) {
+        removeClass(cards[i], "show");
+        if (cards[i].className.indexOf(c) > -1) addClass(cards[i], "show");
     }
 }
 
 
 function addClass (element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) {
-            element.className += " " + arr2[i];
+    var i, classes, newClass;
+    classes = element.className.split(" ");
+    newClass = name.split(" ");
+    for (i = 0; i < newClass.length; i++) {
+        if (classes.indexOf(newClass[i]) == -1) {
+            element.className += " " + newClass[i];
         }
     }
 }
 
 function removeClass (element, name) {
-    var i, arr1, arr2;
-    arr1 = element.className.split(" ");
-    arr2 = name.split(" ");
-    for (i = 0; i < arr2.length; i++) {
-        while (arr1.indexOf(arr2[i]) > -1) {
-            arr1.splice(arr1.indexOf(arr2[i]), 1);
+    var i, classes, remClass;
+    classes = element.className.split(" ");
+    remClass = name.split(" ");
+    for (i = 0; i < remClass.length; i++) {
+        while (classes.indexOf(remClass[i]) > -1) {
+            classes.splice(classes.indexOf(remClass[i]), 1);
         }
     }
-    element.className = arr1.join(" ");
+    element.className = classes.join(" ");
 };
 
+function displayFilters (c) {
+    var window, filters, i;
+    window = document.getElementById('filter-window');
+    filters = document.getElementsByClassName("filter");
+    addClass(window, 'show-filter-window');
+    if (c == "all") c = "";
+    for (i = 0; i < filters.length; i++) {
+        removeClass(filters[i], "show");
+        if (filters[i].className.indexOf(c) > -1) addClass(filters[i], "show");
+    }
+}
+
+function clearFilters () {
+    var window = document.getElementById('filter-window');
+    filterListings("all");
+    displayFilters('all');
+    removeClass(window, 'show-filter-window');
+}
